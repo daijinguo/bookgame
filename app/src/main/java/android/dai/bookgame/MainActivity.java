83e98973f603495de8401a6157563ce4f691e878
dai.jinguo <jinguo504@163.com>
2017-03-31 16:23:56 +0800

    finish the framework to start other activity
    -- in class MainItemViewsUtility can add logic of activity to start

--- app/src/main/java/android/dai/bookgame/MainActivity.java
+++ app/src/main/java/android/dai/bookgame/MainActivity.java
@@ -1,103 +1,1 @@
-package android.dai.bookgame;
-
-import android.app.Activity;
-import android.graphics.Outline;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewOutlineProvider;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import java.util.Random;
-
-public class MainActivity extends Activity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    private final static int viewColors[] = {
-            R.color.colorItemView,
-            R.color.colorItemView1,
-            R.color.colorItemView2,
-            R.color.colorItemView3,
-            R.color.colorItemView4,
-            R.color.colorItemView5,
-            R.color.colorItemView6,
-            R.color.colorItemView7,
-            R.color.colorItemView8,
-            R.color.colorItemView9
-    };
-
-    private static Random msRandom = new Random(viewColors.length);
-
-    private LinearLayout mContainer;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        View mainer = findViewById(R.id.mainer);
-        // demo use setOutlineProvider address
-        // http://blog.csdn.net/u013210620/article/details/50075533
-        mainer.setClipToOutline(true);
-        mainer.setOutlineProvider(new ViewOutlineProvider() {
-            @Override
-            public void getOutline(View view, Outline outline) {
-                //outline.setOval(0, 0, view.getWidth(), view.getHeight());
-                int radius = Math.min(view.getWidth(), view.getHeight()) / 20;
-                outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), radius);
-            }
-        });
-
-        mContainer = (LinearLayout) findViewById(R.id.container);
-
-
-        addView(createView("0001", null));
-        addView(createView("00011", null));
-        addView(createView("000111", null));
-        addView(createView("0001111", null));
-        addView(createView("00011111", null));
-        addView(createView("00011", null));
-        addView(createView("00011", null));
-        addView(createView("000111", null));
-        addView(createView("00101", null));
-        addView(createView("01001", null));
-        addView(createView("00101", null));
-    }
-
-    private void addView(View view) {
-        if (null != view && null != mContainer) {
-            final int N = mContainer.getChildCount();
-            for (int i = 0; i < N; ++i) {
-                if (view == mContainer.getChildAt(i))
-                    return;
-            }
-
-            mContainer.addView(view);
-        }
-    }
-
-    private TextView createView(String desc, View.OnClickListener listener) {
-        TextView view = (TextView) LayoutInflater.from(this).inflate(R.layout.linear_item_view, null);
-        if (null != view) {
-            view.setText(desc);
-            view.setOnClickListener(listener);
-            final int I = msRandom.nextInt(viewColors.length);
-            view.setBackgroundColor(getResources().getColor(viewColors[I]));
-        }
-        return view;
-    }
-
-
-}
+package android.dai.bookgame;import android.app.Activity;import android.graphics.Outline;import android.os.Bundle;import android.view.View;import android.view.ViewOutlineProvider;import android.view.animation.Animation;import android.view.animation.ScaleAnimation;import android.widget.LinearLayout;import android.widget.TextView;import java.util.ArrayDeque;public class MainActivity extends Activity {    // Used to load the 'native-lib' library on application startup.    static {        System.loadLibrary("native-lib");    }    /**     * A native method that is implemented by the 'native-lib' native library,     * which is packaged with this application.     */    public native String stringFromJNI();    private static ArrayDeque<TextView> msItemViews = new ArrayDeque<>();    private LinearLayout mContainer;    private MainItemViewsUtility mUtility;    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_main);        View mainer = findViewById(R.id.mainer);        // demo use setOutlineProvider address        // http://blog.csdn.net/u013210620/article/details/50075533        mainer.setClipToOutline(true);        mainer.setOutlineProvider(new ViewOutlineProvider() {            @Override            public void getOutline(View view, Outline outline) {                //outline.setOval(0, 0, view.getWidth(), view.getHeight());                int radius = Math.min(view.getWidth(), view.getHeight()) / 20;                outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), radius);            }        });        mContainer = (LinearLayout) findViewById(R.id.container);        mUtility = new MainItemViewsUtility(this);        addViewToContainer();    }    private void addViewToContainer() {        if (null == mUtility) return;        ArrayDeque<TextView> views = mUtility.getViews();        if (!views.isEmpty()) {            TextView view = views.remove();            addView(view);            Animation scaleAnimation = new ScaleAnimation(0.1f, 1.0f, 0.1f, 1.0f,                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.0f);            scaleAnimation.setDuration(400);            scaleAnimation.setAnimationListener(new Animation.AnimationListener() {                @Override                public void onAnimationStart(Animation animation) {                }                @Override                public void onAnimationEnd(Animation animation) {                    addViewToContainer();                }                @Override                public void onAnimationRepeat(Animation animation) {                }            });            view.startAnimation(scaleAnimation);        }    }    private void addView(View view) {        if (null != view && null != mContainer) {            final int N = mContainer.getChildCount();            for (int i = 0; i < N; ++i) {                if (view == mContainer.getChildAt(i)) {                    return;                }            }            mContainer.addView(view);        }    }}
---
+++ app/src/main/java/android/dai/bookgame/MainItemViewsUtility.java
@@ -0,0 +1,108 @@
+package android.dai.bookgame;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
+import android.widget.TextView;
+
+import java.util.ArrayDeque;
+import java.util.Random;
+
+class MainItemViewsUtility {
+    private Activity mActivity;
+    private ArrayDeque<TextView> mViews = new ArrayDeque<>();
+
+    private static Random msRandom = new Random();
+    private final static int viewColors[] = {
+            R.color.colorItemView,
+            R.color.colorItemView1,
+            R.color.colorItemView2,
+            R.color.colorItemView3,
+            R.color.colorItemView4,
+            R.color.colorItemView5,
+            R.color.colorItemView6,
+            R.color.colorItemView7,
+            R.color.colorItemView8,
+            R.color.colorItemView9
+    };
+
+    MainItemViewsUtility(Activity activity) {
+        mActivity = activity;
+        mViews.clear();
+
+        testData();
+        postViews();
+    }
+
+    ArrayDeque<TextView> getViews() {
+        return mViews;
+    }
+
+    private TextView createView(String desc, View.OnClickListener listener) {
+        TextView view = (TextView) LayoutInflater.from(mActivity).inflate(R.layout.linear_item_view, null);
+        if (null != view) {
+            view.setText(desc);
+            view.setOnClickListener(listener);
+            final int I = msRandom.nextInt(viewColors.length);
+            view.setBackgroundColor(mActivity.getResources().getColor(viewColors[I]));
+        }
+        return view;
+    }
+
+    private void testData() {
+        mViews.add(createView("exit", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TranslateAnimation translate = new TranslateAnimation(-v.getWidth() / 4, v.getWidth() / 4, 0, 0);
+                translate.setDuration(500);
+                translate.setRepeatCount(4);
+                translate.setRepeatMode(Animation.REVERSE);
+                translate.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        mActivity.finish();
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+                });
+                v.startAnimation(translate);
+            }
+        }));
+
+        mViews.add(createView("click start an animition", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TranslateAnimation translate = new TranslateAnimation(-v.getWidth() / 4, v.getWidth() / 4, 0, 0);
+                translate.setDuration(500);
+                translate.setRepeatCount(2);
+                translate.setRepeatMode(Animation.REVERSE);
+
+                v.startAnimation(translate);
+            }
+        }));
+
+        mViews.add(createView("start camera", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                mActivity.startActivity(intent);
+            }
+        }));
+    }
+
+    private void postViews() {
+
+    }
+}
